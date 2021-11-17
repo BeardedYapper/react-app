34732fd0d3e304915eac93a490223685c9b2a65d
suheily <suheilypergola@gmail.com>
2021-11-17 00:18:47 -0600

    redirect to login if token is null or undefined

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link, useRoutes } from 'react-router-dom';
 import LogIn from './LogIn';
@@ -15,13 +16,12 @@ import Button from '@mui/material/Button';
 import {token} from './utils/global.utils';
 
 function App() {
-  
   const handleLogOut = () => {
     window.localStorage.removeItem('auth_token');
   };
   const Routes = () => {
     let routes = useRoutes([
-      { path: "/", element: <Home /> },
+      { path: "/", element: token ? <Home />: <LogIn/> },
       { path: "/login", element: <LogIn /> },
       { path: "/signUp", element: <SignUp /> },
       { path: "/coursers", element: <Courses /> },
@@ -33,32 +33,32 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <Link style={{textDecoration: 'none', color: 'white'}} to="/">
-            <MenuIcon />
-            </Link>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            APP NAME HERE
-          </Typography>
-          {!token && (
-            <Button color="inherit"> <Link style={{textDecoration: 'none', color: 'white'}} to="/login">Login</Link></Button>
-          )}
-          {token && (
-            <Button color="inherit"> <Link style={{textDecoration: 'none', color: 'white'}} to="/login" onClick={handleLogOut}>Log Out</Link></Button>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-        <Routes />
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <Link style={{textDecoration: 'none', color: 'white'}} to="/">
+              <MenuIcon />
+              </Link>
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              APP NAME HERE
+            </Typography>
+            {!token && (
+              <Button color="inherit"> <Link style={{textDecoration: 'none', color: 'white'}} to="/login">Login</Link></Button>
+            )}
+            {token && (
+              <Button color="inherit"> <Link style={{textDecoration: 'none', color: 'white'}} to="/login" onClick={handleLogOut}>Log Out</Link></Button>
+            )}
+          </Toolbar>
+        </AppBar>
+        </Box>
+          <Routes/>
       </div>
     </Router>
   );
